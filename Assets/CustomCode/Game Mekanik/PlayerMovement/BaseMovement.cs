f5a459dfc8978ae9211365c7ae484d6f9b3e6e6c
sotongCode <game.sugeng@gmail.com>
2020-08-26 11:02:20 +0700

    Basic Movement
    TopDownMovement

---
+++ Assets/CustomCode/Game Mekanik/PlayerMovement/BaseMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovement : MonoBehaviour {
+    // Start is called before the first frame update
+    [Header ("Basic Data")]
+    public KeyCode up;
+    public KeyCode down, left, right;
+
+    public float speed;
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    protected virtual void Move () {
+
+    }
+
+    protected virtual void SetAimDir (Transform rotateObject, Vector2 targetPos) {
+
+    }
+}
---
+++ Assets/CustomCode/Game Mekanik/PlayerMovement/Movement_TopDown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_TopDown : BaseMovement {
+    [Header ("Top Down Data")]
+    public Transform rotateObj;
+
+    private void Update () {
+        SetAimDir (rotateObj,
+            Camera.main.ScreenToWorldPoint (Input.mousePosition));
+
+        Move();
+    }
+    protected override void Move () {
+        if (Input.GetKey (up)) this.transform.Translate (Vector2.up * Time.fixedDeltaTime * speed);
+
+        else if (Input.GetKey (down)) this.transform.Translate (Vector2.down * Time.fixedDeltaTime * speed);
+
+        else if (Input.GetKey (right)) this.transform.Translate (Vector2.right * Time.fixedDeltaTime * speed);
+
+        else if (Input.GetKey (left)) this.transform.Translate (Vector2.left * Time.fixedDeltaTime * speed);
+    }
+
+    protected override void SetAimDir (Transform rotateObject, Vector2 targetPos) {
+        Vector2 objPos = rotateObject.position;
+        Vector2 heading = targetPos - objPos;
+
+        rotateObject.transform.up = heading;
+    }
+}
