1c6608fd837e8c44815bc933f61b1d4624e7734b
sotongCode <game.sugeng@gmail.com>
2020-08-27 15:27:15 +0700

    SaveLoad v1.0
    Fix Save Load for v1.0 ready for use

--- Assets/CustomCode/Game Mekanik/SaveSystem/SaveLoadManager.cs
+++ Assets/CustomCode/Game Mekanik/SaveSystem/SaveLoadManager.cs
@@ -5,52 +5,35 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
-public class SaveLoadManager : MonoBehaviour {
+public class SaveLoadManager{
+    // public static void SaveData (ExampleData rawData, string path, string fileName) {
+    //     BinaryFormatter bf = new BinaryFormatter ();
+    //     FileStream file = File.Create (path + "/" + fileName);
 
-    ExampleData dataOnGame;
+    //     ExampleData data = new ExampleData ();
 
-    static SaveLoadManager instance;
+    //     data.intVal = rawData.intVal;
+    //     data.stringVal = rawData.stringVal;
+    //     data.floatVal = rawData.floatVal;
 
-    private void Awake () {
-        if (instance == null) {
-            DontDestroyOnLoad (gameObject);
-            instance = this;
-        } else if (instance != this) {
-            Destroy (gameObject);
-        }
-    }
+    //     bf.Serialize (file, data);
+    //     file.Close ();
+    // }
+    // public static void LoadData (string path, string fileName) {
+    //     if (File.Exists (path + "/" + fileName)) {
+    //         BinaryFormatter bf = new BinaryFormatter ();
+    //         FileStream file = File.Open (path + "/" + fileName, FileMode.Open);
+    //         ExampleData data = (ExampleData) bf.Deserialize (file);
+    //         file.Close ();
 
-    private void Start () {
+    //         instance.dataOnGame = data;
 
-    }
-    public static void SaveData (ExampleData rawData, string path, string fileName) {
-        BinaryFormatter bf = new BinaryFormatter ();
-        FileStream file = File.Create (path + "/" + fileName);
-
-        ExampleData data = new ExampleData ();
-
-        data.intVal = rawData.intVal;
-        data.stringVal = rawData.stringVal;
-        data.floatVal = rawData.floatVal;
-
-        bf.Serialize (file, data);
-        file.Close ();
-    }
-    public static void LoadData (string path, string fileName) {
-        if (File.Exists (path + "/" + fileName)) {
-            BinaryFormatter bf = new BinaryFormatter ();
-            FileStream file = File.Open (path + "/" + fileName, FileMode.Open);
-            ExampleData data = (ExampleData) bf.Deserialize (file);
-            file.Close ();
-
-            instance.dataOnGame = data;
-
-            //OR
-            // instance.dataOnGame.floatVal = data.floatVal;
-            // instance.dataOnGame.stringVal = data.stringVal;
-        }
-    }
-    public static void SaveData1<T> (T rawData, string path, string fileName) {
+    //         //OR
+    //         // instance.dataOnGame.floatVal = data.floatVal;
+    //         // instance.dataOnGame.stringVal = data.stringVal;
+    //     }
+    // }
+    public static void SaveData<T> (T rawData, string path, string fileName) {
         BinaryFormatter bf = new BinaryFormatter ();
         FileStream file = File.Create (path + "/" + fileName);
 
@@ -59,7 +42,7 @@ public class SaveLoadManager : MonoBehaviour {
         bf.Serialize (file, data);
         file.Close ();
     }
-    public static void LoadData2<T> (string path, string fileName, out T targetData) {
+    public static void LoadData<T> (string path, string fileName, out T targetData) {
         targetData = default(T);
 
         if (File.Exists (path + "/" + fileName)) {
@@ -76,11 +59,4 @@ public class SaveLoadManager : MonoBehaviour {
         }
 
     }
-}
-
-[Serializable]
-public class ExampleData {
-    public int intVal;
-    public string stringVal;
-    public float floatVal;
 }
---
+++ Assets/CustomCode/Game Mekanik/SaveSystem/SaveLoadTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadTest : MonoBehaviour {
+    // Start is called before the first frame update
+
+    [Header ("Save Data")]
+    public ExampleData data1 = new ExampleData (1, "1", 1.1f);
+    public ExampleData2 data2 = new ExampleData2 ("2");
+
+    [Header ("Load Data")]
+    public ExampleData loadData1;
+    public ExampleData2 loadData2;
+
+    public string path;
+
+    [SerializeField] string saveData1 = "saveData1.dat";
+    [SerializeField] string saveData2 = "saveData2.dat";
+    void Start () {
+        path = Application.dataPath;
+    }
+
+    public void MakeSave () {
+        SaveLoadManager.SaveData<ExampleData> (data1, path, saveData1);
+        SaveLoadManager.SaveData<ExampleData2> (data2, path, saveData2);
+    }
+
+    public void LoadSaveData () {
+        SaveLoadManager.LoadData<ExampleData> (path, saveData1, out ExampleData resultData);
+        SaveLoadManager.LoadData<ExampleData2> (path, saveData2, out ExampleData2 resultData2);
+        loadData1 = resultData;
+        loadData2 = resultData2;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
+
+[Serializable]
+public class ExampleData {
+    public int intVal;
+    public string stringVal;
+    public float floatVal;
+
+    public ExampleData (int intDat, string stringDat, float floatDat) {
+        intVal = intDat;
+        stringVal = stringDat;
+        floatVal = floatDat;
+    }
+    public ExampleData () {
+
+    }
+}
+
+[Serializable]
+public class ExampleData2 {
+    public string data;
+
+    public ExampleData2 (string input) {
+        data = input;
+    }
+    public ExampleData2 () {
+
+    }
+}
