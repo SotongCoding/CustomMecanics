4eaad267a2fb693801fddeb9dfbc968be9293374
sotongCode <game.sugeng@gmail.com>
2020-08-27 16:02:48 +0700

    Save System v1.1
    Adding for List data

--- Assets/CustomCode/Game Mekanik/SaveSystem/SaveLoadManager.cs
+++ Assets/CustomCode/Game Mekanik/SaveSystem/SaveLoadManager.cs
@@ -6,33 +6,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class SaveLoadManager{
-    // public static void SaveData (ExampleData rawData, string path, string fileName) {
-    //     BinaryFormatter bf = new BinaryFormatter ();
-    //     FileStream file = File.Create (path + "/" + fileName);
-
-    //     ExampleData data = new ExampleData ();
-
-    //     data.intVal = rawData.intVal;
-    //     data.stringVal = rawData.stringVal;
-    //     data.floatVal = rawData.floatVal;
-
-    //     bf.Serialize (file, data);
-    //     file.Close ();
-    // }
-    // public static void LoadData (string path, string fileName) {
-    //     if (File.Exists (path + "/" + fileName)) {
-    //         BinaryFormatter bf = new BinaryFormatter ();
-    //         FileStream file = File.Open (path + "/" + fileName, FileMode.Open);
-    //         ExampleData data = (ExampleData) bf.Deserialize (file);
-    //         file.Close ();
-
-    //         instance.dataOnGame = data;
-
-    //         //OR
-    //         // instance.dataOnGame.floatVal = data.floatVal;
-    //         // instance.dataOnGame.stringVal = data.stringVal;
-    //     }
-    // }
+    
     public static void SaveData<T> (T rawData, string path, string fileName) {
         BinaryFormatter bf = new BinaryFormatter ();
         FileStream file = File.Create (path + "/" + fileName);
@@ -52,10 +26,29 @@ public class SaveLoadManager{
             file.Close ();
 
             targetData = data;
+        }
 
-            //OR
-            // instance.dataOnGame.floatVal = data.floatVal;
-            // instance.dataOnGame.stringVal = data.stringVal;
+    }
+
+    public static void SaveData<T> (List<T> rawData, string path, string fileName) {
+        BinaryFormatter bf = new BinaryFormatter ();
+        FileStream file = File.Create (path + "/" + fileName);
+
+        List<T> data = rawData;
+
+        bf.Serialize (file, data);
+        file.Close ();
+    }
+    public static void LoadData<T> (string path, string fileName, out List<T> targetData) {
+        targetData = default(List<T>);
+
+        if (File.Exists (path + "/" + fileName)) {
+            BinaryFormatter bf = new BinaryFormatter ();
+            FileStream file = File.Open (path + "/" + fileName, FileMode.Open);
+            List<T> data = (List<T>) bf.Deserialize (file);
+            file.Close ();
+
+            targetData = data;
         }
 
     }
--- Assets/CustomCode/Game Mekanik/SaveSystem/SaveLoadTest.cs
+++ Assets/CustomCode/Game Mekanik/SaveSystem/SaveLoadTest.cs
@@ -10,33 +10,57 @@ public class SaveLoadTest : MonoBehaviour {
     public ExampleData data1 = new ExampleData (1, "1", 1.1f);
     public ExampleData2 data2 = new ExampleData2 ("2");
 
+    public List<ExampleData> listData1 = new List<ExampleData> () {
+        new ExampleData (101, "101", 1.01f),
+        new ExampleData (102, "102", 1.02f),
+        new ExampleData (103, "103", 1.03f)
+    };
+    public List<ExampleData2> listData2 = new List<ExampleData2> () {
+        new ExampleData2 ("1"),
+        new ExampleData2 ("2"),
+        new ExampleData2 ("3")
+    };
+
     [Header ("Load Data")]
     public ExampleData loadData1;
     public ExampleData2 loadData2;
+
+    public List<ExampleData> loadListData1;
+    public List<ExampleData2> loadListData2;
 
     public string path;
 
     [SerializeField] string saveData1 = "saveData1.dat";
     [SerializeField] string saveData2 = "saveData2.dat";
+    [SerializeField] string LsaveData1 = "listSaveData1.dat";
+    [SerializeField] string LsaveData2 = "listSaveData2.dat";
     void Start () {
-        path = Application.dataPath;
+        path = Application.dataPath + "/CustomCode/Game Mekanik/SaveSystem";
     }
 
     public void MakeSave () {
+        //Individual Data
         SaveLoadManager.SaveData<ExampleData> (data1, path, saveData1);
         SaveLoadManager.SaveData<ExampleData2> (data2, path, saveData2);
+
+        //ListData
+        SaveLoadManager.SaveData<ExampleData> (listData1, path, LsaveData1);
+        SaveLoadManager.SaveData<ExampleData2> (listData2, path, LsaveData2);
+
     }
 
     public void LoadSaveData () {
         SaveLoadManager.LoadData<ExampleData> (path, saveData1, out ExampleData resultData);
         SaveLoadManager.LoadData<ExampleData2> (path, saveData2, out ExampleData2 resultData2);
+
+        SaveLoadManager.LoadData<ExampleData> (path, LsaveData1, out List<ExampleData> LresultData);
+        SaveLoadManager.LoadData<ExampleData2> (path, LsaveData2, out List<ExampleData2> LresultData2);
+
+        loadListData1 = LresultData;
+        loadListData2 = LresultData2;
+
         loadData1 = resultData;
         loadData2 = resultData2;
-    }
-
-    // Update is called once per frame
-    void Update () {
-
     }
 }
 
