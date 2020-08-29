1a3dc88cef67f17a271ad26f800d9511001b03ef
sotongCode <game.sugeng@gmail.com>
2020-08-29 10:40:41 +0700

    Fix Paralax + Movement Fix and Adding some Function

---
+++ Assets/CustomCode/Game Mekanik/Paralax/ParalaxEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxEffect : MonoBehaviour {
+    public List<ParalaxObject> paralaxObj;
+    public bool onPlayerMove;
+    BaseMovement movement;
+
+    private void Awake () {
+        movement = FindObjectOfType<BaseMovement> ();
+    }
+
+    private void FixedUpdate () {
+        onPlayerMove = movement.GetMoveStatus ();
+
+        if (onPlayerMove) {
+            foreach (ParalaxObject item in paralaxObj) {
+                item.Move (Vector2.left * movement.GetMoveDiriction().x);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class ParalaxObject {
+    public Transform obj;
+    public float speed;
+
+    public void Move (Vector2 direction) {
+        obj.transform.Translate (direction * speed * Time.fixedDeltaTime);
+    }
+}
--- Assets/CustomCode/Game Mekanik/PlayerMovement/BaseMovement.cs
+++ Assets/CustomCode/Game Mekanik/PlayerMovement/BaseMovement.cs
@@ -9,20 +9,57 @@ public class BaseMovement : MonoBehaviour {
     public KeyCode down, left, right;
 
     public float speed;
-    void Start () {
 
+    private bool isMove;
+    private Vector2 moveDir;
+    [Header ("Top Down Data")]
+    public Transform rotateObj;
+    private void Update () {
+        SetAimDir (rotateObj,
+            Camera.main.ScreenToWorldPoint (Input.mousePosition));
+
+        Move ();
+    }
+    void Move () {
+        if (Input.GetKey (up)) {
+            this.transform.Translate (Vector2.up * Time.fixedDeltaTime * speed);
+            moveDir += Vector2.up;
+
+        } else if (Input.GetKey (down)) {
+            this.transform.Translate (Vector2.down * Time.fixedDeltaTime * speed);
+            moveDir += Vector2.down;
+
+        } else if (Input.GetKey (right)) {
+            this.transform.Translate (Vector2.right * Time.fixedDeltaTime * speed);
+            moveDir += Vector2.right;
+
+        } else if (Input.GetKey (left)) {
+            this.transform.Translate (Vector2.left * Time.fixedDeltaTime * speed);
+            moveDir += Vector2.left;
+        }
+
+        if (Input.GetKey (up) || Input.GetKey (down) || Input.GetKey (right) || Input.GetKey (left)) {
+            isMove = true;
+        } else {
+            moveDir = Vector2.zero;
+            isMove = false;
+        }
+    }
+    public bool GetMoveStatus () {
+        return isMove;
+    }
+    public Vector2 GetMoveDiriction () {
+        int x = moveDir.x > 0 ? 1 : moveDir.x < 0 ? -1 : 0;
+        int y = moveDir.y > 0 ? 1 : moveDir.y < 0 ? -1 : 0;
+        return new Vector2 (x, y); //moveDir.normalized;
     }
 
-    // Update is called once per frame
-    void Update () {
+    void SetAimDir (Transform rotateObject, Vector2 targetPos) {
+        if (rotateObj != null) {
+            Vector2 objPos = rotateObject.position;
+            Vector2 heading = targetPos - objPos;
 
-    }
-
-    protected virtual void Move () {
-
-    }
-
-    protected virtual void SetAimDir (Transform rotateObject, Vector2 targetPos) {
-
+            rotateObject.transform.up = heading;
+        }
     }
 }
