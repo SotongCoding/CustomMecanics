ef270cf9aad0d53c21eef223d8b39521f717b20f
sotongCode <game.sugeng@gmail.com>
2020-09-01 19:28:27 +0700

    Font Change Script
    Make Sample for Change TMPProText without html code

---
+++ Assets/CustomCode/UI/CutomUIText/CustomTextControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CustomTextControl : MonoBehaviour {
+    public TextMeshProUGUI targetText;
+    // Start is called before the first frame update
+    void Start () {
+        targetText.text =
+            CustomUIText.TextColor ("This Color text", Color.green) + "\n" +
+            CustomUIText.TextSize ("Change size Text", 200) + "\n" +
+            CustomUIText.B_I_U_Text ("this Bold", CustomUIText.TextType.bold) + "\n" +
+            CustomUIText.B_I_U_Text ("this Italic", CustomUIText.TextType.italic) + "\n" +
+            CustomUIText.B_I_U_Text ("this UnderLine", CustomUIText.TextType.underline) + "\n\n" +
+            CustomUIText.ChangeText ("this ALGERIAN Text", "ALGER SDF") + "\n" +
+            CustomUIText.ChangeText ("this ITECDSCR", "ITCEDSCR SDF") + "\n";
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Assets/CustomCode/UI/CutomUIText/CustomUIText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomUIText {
+    public static string TextColor (string text, Color32 color) {
+        string coloroHex = ColorUtility.ToHtmlStringRGB (color);
+        return "<color=#" + coloroHex + ">" + text + "</color>";
+    }
+    public static string TextSize (string text, int size) {
+        return "<size=" + size + ">" + text + "</size>";
+    }
+    public static string B_I_U_Text (string text, TextType type) {
+        if (type == TextType.bold) {
+            return "<b>" + text + "</b>";
+        } else if (type == TextType.italic) {
+            return "<i>" + text + "</i>";
+        } else if (type == TextType.underline) {
+            return "<u>" + text + "</u>";
+        }
+        return "";
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="fontAssetName">Look at TMPProSetting for Font asset Placement</param>
+    /// <returns></returns>
+    public static string ChangeText (string text, string fontAssetName) {
+        return "<font=" + fontAssetName + ">" + text + "</font>";
+    }
+
+    public enum TextType {
+        bold,
+        italic,
+        underline
+    }
+}
